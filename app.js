ec13ad1b89fdf498eb1f2cd5de7b2818a3b08f57
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-12 12:36:29 -0400

    fix

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+require('dotenv').config()
+const connectDB = require('./db/connect')
 const app = express()
 
 app.get('/', (req, res) => {
@@ -8,9 +10,17 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 3000
 
-const server = app.listen(port, () => {
-  console.log('Server is listening on port 3000...')
-})
+const start = async () => {
+  try {
+    const result = await connectDB(process.env.MONGO_URI)
+    console.log('Connected to MongoDB...', result.connections[0].name)
+    app.listen(port, console.log(`Server is listening on port ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const server = start()
 
 module.exports = {
   app,
