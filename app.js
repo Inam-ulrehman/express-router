f8860bb81dd5e37c036138b49dc6bcb0c21f1cbd
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-12 11:23:44 -0400

    update

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ app.get('/', (req, res) => {
   res.send(`Client IP: ${clientIP}`)
 })
 
-module.exports = app // Export the app for testing
-
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`)
+const server = app.listen(3000, () => {
+  console.log('Server is listening on port 3000...')
 })
+
+module.exports = {
+  app,
+  server,
+}
--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 const request = require('supertest')
-const app = require('./app')
+const { app, server } = require('./app')
 
 describe('Server', () => {
   it('should respond with status 200', async () => {
     const response = await request(app).get('/')
     expect(response.status).toBe(200)
+  })
+
+  afterAll((done) => {
+    server.close(done)
   })
 })
