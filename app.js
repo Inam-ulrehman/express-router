4c48ecec99ecc94e80616c9d2aad1a19cb0c3aa1
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2023-06-13 08:25:01 -0400

    update packages

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const sanitizeInput = require('./middleware/xss')
+const helmet = require('helmet')
+const cors = require('cors')
+const rateLimiter = require('express-rate-limit')
 
 const app = express()
+app.use(express.json())
+
+// extra security packages
+
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // limit each IP to 1000 requests per windowMs
+  })
+)
+app.use(helmet())
+app.use(cors())
+app.use(sanitizeInput)
 
 // Middleware
 
